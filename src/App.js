c2ff9031a085b09fef18a45a18481ad82e534c78
ios10 <trikshotzz05@gmail.com>
2022-06-01 11:41:59 -0700

    working on winner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Square from './components/Square'
 import './App.css'
+import Winner from './components/Winner'
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +10,33 @@ class App extends Component {
       squares: [null, null, null, null, null, null, null, null, null],
       player: 1,
     }
+  }
+
+// create a function called calculateWinner
+  // function will take in a variable of winning arrays
+  // use a for loop to go through the index's of squares.
+  // give an array of numbers marked as letters a, b ,c
+// create an if conditional to set the winning match
+  // return the array of matching values
+  // return null
+  calculateWinner = (squareArray) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squareArray[a] && squareArray[a] === squareArray[b] && squareArray[b] === squareArray[c]) {
+        return squareArray[a];
+      }
+    }
+    return null;
   }
 
  markSquare = (index) => {
@@ -21,12 +49,14 @@ class App extends Component {
     squares[index] = "⭕️"
     player = 1
   this.setState({squares: squares, player: player})
-  }
+  } 
+  
  }
 
   render() {
     return(
       <>
+
         <h1 className='heading'>Tic Tac Toe</h1>
         <div className="grid">
           {this.state.squares.map((value, index) => {
@@ -39,8 +69,19 @@ class App extends Component {
             } 
           )}
         </div>
+        <div>
+            <Winner
+            squareArray = {this.state.squares}
+            winCheck = {this.calculateWinner}
+            />
+
+        </div>
       </>
     )
   }
 }
+
+
+
+
 export default App
